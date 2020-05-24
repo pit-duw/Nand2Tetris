d43f15f5824340614291a16b895fc4efc02f313c
Pit Duwentaester <pit.duw@gmail.com>
2020-05-25 00:27:35 +0200

    Completed chapter 4 project

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,81 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// Initialize variable screenend as start of screen + number of pixel-blocks -1
+@8192
+D=A
+@SCREEN
+D=D+A
+D=D-1
+@screenend
+M=D
+
+// Initial position is at screen end
+@pos
+M=D
+
+
+// Main loop
+(MAIN)
+
+    // Read Keyboard input
+    @KBD
+    D=M
+
+    // Blacken one block if something is pressed
+    @BLACKEN
+    D;JGT
+
+    // Clear one block if nothing is pressed
+    @CLEAR
+    D;JEQ
+
+
+
+
+// Blacken one pixel-block at position pos and decrement pos
+(BLACKEN)
+
+    // Read pos into A and D
+    @pos
+    AD=M
+
+    // Set pixels at pos to black
+    M=-1
+
+    // Check if start of screen is reached, go back to main if it is
+    @SCREEN
+    D=D-A
+    @MAIN
+    D;JEQ
+
+    // Decrement pos if start of screen is not reached yet and return to main
+    @pos
+    M=M-1
+    @MAIN
+    0;JMP
+
+
+
+// Clear one pixel-block at position pos and increment pos
+(CLEAR)
+
+    // Read pos into A and D
+    @pos
+    AD=M
+
+    // Clear pixels at pos
+    M=0
+
+    // Check if end of screen is reached, go back to main if it is
+    @screenend
+    D=D-M
+    @MAIN
+    D;JEQ
+
+    // Increment pos if end of screen is not reached yet and return to main
+    @pos
+    M=M+1
+    @MAIN
+    0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,46 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// Create variable i and set to value of R0
+@0
+D=M     
+@i
+M=D
+
+// Create variable res and set to 0
+@res
+M=0
+
+// Start of loop -> loop R0 times by decrementing i after each iteration
+// In each iteration add R1 to res 
+(LOOP)
+
+    // End loop if i = 0
+    @i
+    D=M
+    @END
+    D;JEQ
+
+    // Read value from R1 and add to res 
+    @1
+    D=M
+    @res
+    M=M+D
+    
+    // Decrement i
+    @i
+    M=M-1
+
+    // Unconditional jump back to loop beginning
+    @LOOP
+    0;JMP
+
+// End of loop
+(END)
+
+// Write result to R2
+@res
+D=M
+@2
+M=D
